2c8d4ec6a39891dd5d12980faf161b8d4977068a
Andrew Redfern <redfern_uk@hotmail.com>
2018-05-08 15:40:34 +0100

    Boditrax-UsingSwitch
    Boditrax-UsingSwitch

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in super hero name to see their nick name:");
+            string userValue = Console.ReadLine();
+
+            switch (userValue)
+            {
+                case "Bat Man":
+                    Console.WriteLine("The Caped Crusader");
+                    break;
+                case "Super Man":
+                    Console.WriteLine("The man of Steel");
+                    break;
+                case "Green Lantern":
+                    Console.WriteLine("The Emrald Knight");
+                    break;
+                default:
+                    Console.WriteLine("This entery does not compute");
+                    break;                
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
